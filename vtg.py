89033d1158b4797a77696010732080f6ca5bf7ea
Nastya2228 <128375621+Nastya2228@users.noreply.github.com>
2023-10-02 14:16:58 +0300

    Add files via upload

---
+++ vtg.py
@@ -0,0 +1,73 @@
+
+
+# #
+# a= int(input())
+# b= a + 1
+# c = a + 2
+# print(a,b,c ,sep = "\n")
+# #
+
+# q = int(input())
+# w = int(input())
+# e = int(input())
+# print(q + w + e)
+#
+# a = int(input())
+# volume = a ** 3
+# square = 6 * a ** 2
+# print("Объем",volume)
+# print("Площадь полной поверхности ",square)
+
+
+# a = int(input())
+# b = int(input())
+# result = 3 * (a + b) ** 3 + 275 * b ** 2 - 127 * a - 41
+# print(result)
+
+# a = int(input())
+# previous = a - 1
+# next = a + 1
+# print(f"Следующее за числом {a} число:" ,next)
+# print(f"Для числа {a} предыдущее число: ",previous)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# result = (a + b + c + d)*3
+# print(result)
+
+
+# a = int(input())
+# d = int(input())
+# n = int(input())
+# result = a + ( n - 1) * d
+# print(result)
+
+# a = int(input())
+# print(a,2*a,3*a,4*a,5*a,sep="---")
+
+# n = int(input())
+# k = int(input())
+# print(k//n)
+# print(k%n)
+
+# n = int(input())
+# print((n//2) + n%2)
+
+# n = int(input())
+# b = n//60
+# c = n%60
+# print(f"{n} мин - это {b} час {c} минут")
+
+
+
+# ki = int(input())
+# kai = str(ki)
+#
+# num1 = int(kai[0]) + int(kai[1]) + int(kai[2])
+# num2 = int(kai[0]) * int(kai[1]) * int(kai[2])
+# print(num1, num2)
+
+a = int(input())
+print()
